310db2346c4a430154bae7402935b2d73b41fdfb
R2 <chayanin.c@round2solutions.com>
2022-01-22 16:02:03 +0700

    time is ticking

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -5,14 +5,19 @@ using UnityEngine.UIElements;
 
 public class UIController : MonoBehaviour
 {
-    public Button SFXButton;
-    public Button optionsButton;
+    private Button SFXButton;
+    private Button optionsButton;
+    private Label timeLabel;
+    private Label miceLabel;
+    private Label hpLabel;
+    private bool isPuased;
     // Start is called before the first frame update
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
         SFXButton = root.Q<Button>("sfx-button");
         optionsButton = root.Q<Button>("options-button");
+        timeLabel = root.Q<Label>("time-label");
 
         SFXButton.clicked += SFXPressed;
         optionsButton.clicked += OptionsPressed;
@@ -26,5 +31,22 @@ public class UIController : MonoBehaviour
     void OptionsPressed()
     {
         Debug.Log("Options clicked");
+    }
+
+    public void setTime(int time){
+        timeLabel.text = formatTime(time);
+    }
+
+    private string formatTime(int time){
+        string min = (time/60) < 10 ? ("0" + (time/60).ToString()) : (time/60).ToString();
+        string sec = (time%60) < 10 ? ("0" + (time%60).ToString()) : (time%60).ToString();
+        
+        return $"{min}:{sec}";
+    }
+
+    void Update()
+    {
+        // Debug.Log(Time.time);
+        setTime(Mathf.FloorToInt(Time.time));
     }
 }
---
+++ Assets/Scripts/UI/testDelegater.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testDelegater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UIController uiController;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(Time.time);
+        // uiController.setTime(Mathf.FloorToInt(Time.time));
+    }
+}
