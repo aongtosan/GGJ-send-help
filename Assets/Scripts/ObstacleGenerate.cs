0e7fd81ccebe9930837ab2436775e4f30a35f680
aongtosan <aongtosan@gmail.com>
2022-01-22 14:00:38 +0700

    refactory ObsGen

--- Assets/Scripts/ObstacleGenerate.cs
+++ Assets/Scripts/ObstacleGenerate.cs
@@ -9,9 +9,13 @@ public class ObstacleGenerate : MonoBehaviour
     GameObject obsPref;
     void Start()
     {
-        for(int i=0; i < 10; i++)
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        for (int i=0; i < 10; i++)
         {
-            Vector3 obsPosition = new Vector3(Random.Range(-15, 15), 1 , Random.Range(-15, 15));
+            Vector3 obsPosition = new Vector3(
+                Random.Range( -(mesh.bounds.size.x/2) * (transform.localScale.x - 1), (mesh.bounds.size.x / 2) * (transform.localScale.x - 1))
+                , 1 ,
+                  Random.Range(-(mesh.bounds.size.z / 2) * (transform.localScale.z - 1), (mesh.bounds.size.z / 2) * (transform.localScale.z - 1)));
             Instantiate(
                 obsPref,obsPosition,obsPref.transform.rotation
             );
