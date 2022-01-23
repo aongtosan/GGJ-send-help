fc8c7c2beef80a0defd927222a40816801d5a534
Michael Delfino <michael.a.delfino@gmail.com>
2022-01-23 10:35:21 +0700

    Make food items spawn after old ones are gone

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     public GameObject startScreen;
     public UnityEvent OnGameStart;
+
+    public GameObject[] foodPrefabs;
+
+    [SerializeField]
+    private int maxFood = 4;
     void Start()
     {
         
@@ -16,6 +21,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject[] food = GameObject.FindGameObjectsWithTag("Food");
+        if (food.Length < maxFood) {
+            GameObject foodToSpawn = foodPrefabs[Random.Range(0, foodPrefabs.Length)];
+            Vector3 spawnLocation = new Vector3(Random.Range(-19f, 19f), 0f, Random.Range(-19f, 19f));
+            Instantiate(foodToSpawn, spawnLocation, foodToSpawn.transform.rotation);
+        }
     }
 }
